f0ba3ef4bd3a74588f8eb34314244f07ebf549d7
Jason Webb <zen.webb@gmail.com>
2019-02-27 20:05:23 -0600

    Implementing random rings of points, invert + point visibility flags

--- basic/js/entry.js
+++ basic/js/entry.js
@@ -1,5 +1,8 @@
 import { Delaunay } from "d3-delaunay";
 
+let showPoints = false,
+  invertColors = false;
+
 const sketch = function (p5) {
   /*
     Setup
@@ -7,17 +10,7 @@ const sketch = function (p5) {
   */
   p5.setup = function () {
     p5.createCanvas(window.innerWidth, window.innerHeight);
-
-    let points = [];
-
-    for (let i = 0; i < 100; i++) {
-      points.push([
-        p5.random(window.innerWidth),
-        p5.random(window.innerHeight)
-      ]);
-    }
-
-    drawVoronoi(points);
+    generateNewShape();
   }
 
   /*
@@ -31,7 +24,23 @@ const sketch = function (p5) {
     Key released handler
     ====================
   */
-  p5.keyReleased = function () {}
+  p5.keyReleased = function () {
+    switch(p5.key) {
+      case ' ':
+        generateNewShape();
+        break;
+
+      case 'p':
+        showPoints = !showPoints;
+        generateNewShape();
+        break;
+
+      case 'i':
+        invertColors = !invertColors;
+        generateNewShape();
+        break;
+    }
+  }
 
 
   /*
@@ -51,6 +60,67 @@ const sketch = function (p5) {
       }
 
       p5.endShape();
+    }
+  }
+
+  // Create an array of points (coordinate pairs)
+  function createPointsRing(centerX, centerY, radius, numPoints) {
+    let points = [];
+
+    for(let i = 0; i < numPoints; i++) {
+      points.push([
+        centerX + radius * Math.cos(p5.radians((360 / numPoints) * i)),
+        centerY + radius * Math.sin(p5.radians((360 / numPoints) * i))
+      ]);
+    }
+
+    return points;
+  }
+  
+  function generateNewShape() {
+    let points = [];
+    let lastRadius = p5.random(10,30);
+
+    // Generate set of points for Voronoi diagram
+    for(let i = 0; i < parseInt(p5.random(5,20)); i++) {
+      let numPoints;
+      if(i < 3) {
+        numPoints = parseInt(p5.random(3,10));
+      } else {
+        numPoints = parseInt(p5.random(20,50));
+      }
+
+      let newPoints = createPointsRing(window.innerWidth/2, window.innerHeight/2, lastRadius, numPoints);
+      points = points.concat(newPoints);
+      
+      lastRadius += p5.random(20,80);
+    }
+
+    // Draw the Voronoi diagram
+    if(invertColors) {
+      p5.background(0);
+      p5.stroke(255);
+    } else {
+      p5.background(255);
+      p5.stroke(0);
+    }
+
+    p5.noFill();
+    drawVoronoi(points);
+
+    // Draw the points
+    if(showPoints) {
+      p5.noStroke();
+
+      if(invertColors) {
+        p5.fill(50);
+      } else {
+        p5.fill(200);
+      }
+  
+      for(let point of points) {
+        p5.ellipse(point[0], point[1], 5);
+      }
     }
   }
 }
